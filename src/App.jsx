83938a5e0b687ac5f10afa44101132bd3449c019
Tim van der Kleijn <timkleijn@gmail.com>
2023-04-23 14:10:34 +0200

    Changed browserrouter to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Navbar } from './components/navbar'
 import { Cart } from './pages/cart/cart'

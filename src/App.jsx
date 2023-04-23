6b02ec3d7fc33b891b81410265bbccabc8843700
Tim van der Kleijn <timkleijn@gmail.com>
2023-04-23 13:58:44 +0200

    change routing

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/webshop' element={<Shop />} />
+          <Route path='/shop' element={<Shop />} />
 
           <Route path='/cart' element={<Cart />} />
         </Routes>
